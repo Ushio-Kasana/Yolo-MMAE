aa11a1a35a71cd726c547fea22ce2472def226b4
bn <n>
2026-05-06 23:03:10 +0930

    Added copyright

--- Macos-Setup-systemwide.sh
+++ Macos-Setup-systemwide.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
+# YOLO-MMAE - Video Annotator & Auto-Tracker
+# Copyright (C) 2026  Ushio-Kasana
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #Check if Brew is installed
 echo "Checking if homebrew is currently installed"
 if command -v brew &>/dev/null; then
